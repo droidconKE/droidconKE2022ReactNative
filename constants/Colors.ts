7116b5051d804f90811840e75c54ee63819ee41c
Sam <wafulasammie@gmail.com>
2022-09-06 10:52:46 +0300

    feature: add folder structure

---
+++ constants/Colors.ts
@@ -0,0 +1,10 @@
+// note: this helps modify / alter colors from a central point
+
+enum colors {
+    PRIMARY_COLOR = '#15A8BC',
+    SECONDARY_COLOR = '#55D187',
+    LIGHT_THEME_BACKGROUND_COLOR = "#F4F5F7",
+    DARK_THEME_BACKGROUND_COLOR = "#000000",
+    LIGHT_THEME_TEXT_COLOR = '#4D4D4D',
+    DARK_THEME_TEXT_COLOR = '#4D4D4D',
+}
---
+++ constants/Properties.ts
@@ -0,0 +1,7 @@
+// includes globally reusable CSS properties like border-radius, box-shadows e.t.c
+
+enum properties {
+    BORDER_RADIUS = 8,
+    BORDER_RADIUS_MAX = 50,
+    BOX_SHADOW = "",
+}
