9270e83691135a68741a464cc64cb0b494205b62
Brian Wachira <brianwachira7@gmail.com>
2022-09-13 17:16:39 +0300

    [feat] : export colors

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,6 +1,6 @@
 // note: this helps modify / alter colors from a central point
 
-enum colors {
+export enum colors {
     LIGHT_THEME_BACKGROUND_COLOR = "#F4F5F7",
     DARK_THEME_BACKGROUND_COLOR = "#000000",
     LIGHT_THEME_TEXT_COLOR = '#4D4D4D',
