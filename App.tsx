f2645fa551df029cb29128d86c8abdd6e800fd98
brianwachira <brianwachira7@gmail.com>
2022-09-16 14:16:39 +0300

    [chore] : remove unused imports

--- App.tsx
+++ App.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
 import { store } from "./src/state/store";
 import { NavigationContainer } from "@react-navigation/native";
