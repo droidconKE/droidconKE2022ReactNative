02fa0f78db1fb6ab9a94cb37ac21844ea8908237
Derick Muli <derickmuli@Dericks-MacBook-Pro.local>
2022-09-17 10:27:41 +0300

    random

---
+++ src/screens/SignInScreen.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+
+export default function SignUpScreen() : JSX.Element {
+    return (<></>)
+}
---
+++ src/screens/SignUpScreen.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+
+export default function SignUpScreen() : JSX.Element {
+    return (<>
+    <View style={{width:"412px" , height:"179" ,backgroundColor:"blue"}}></View>
+
+    </>)
+}
