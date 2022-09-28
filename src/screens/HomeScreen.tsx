bcf5086f8ccde1b377e12c1ba485b1ef88cde727
brianwachira <brianwachira7@gmail.com>
2022-09-28 13:24:11 +0300

    [feat] : home-not-logged-in screen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -6,12 +6,15 @@ import { ParamListBase } from '@react-navigation/native';
 import { colors } from '../constants/Colors';
 import LockIcon from '../assets/icons/LockIcon';
 import { fonts } from '../assets/fonts/fonts';
+import Android254Icon from '../assets/icons/Android254Icon';
+import AppsLabIcon from '../assets/icons/AppsLabIcon';
+import TiskosIcon from '../assets/icons/TiskosIcon';
 
 const HomeScreen = ({navigation}: NativeStackScreenProps<ParamListBase, screen_names.HOME, undefined>) => {
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.header}>
-                <Text>droidconke</Text>
+                <Text>droidconke-icon</Text>
                 <TouchableOpacity style={styles.iconWrapper}>
                     <LockIcon/>
                 </TouchableOpacity>
@@ -19,12 +22,12 @@ const HomeScreen = ({navigation}: NativeStackScreenProps<ParamListBase, screen_n
             <View style={styles.marginSeparator}>
                 <Text style={styles.welcomeText}>Welcome to the largest Focused Android Developer community in Africa!</Text>
             </View>
-            <ScrollView>
+            <ScrollView showsVerticalScrollIndicator={false}>
                 <View style={styles.marginSeparator}>
-                    <Image source={{uri: '../assets/droidconbanner.png'}}/>
+                    <Image source={require('../assets/droidconkebanner.png')} resizeMode="contain" style={styles.droidconkeBanner}/>
                 </View>
-                <View style={[styles.cfpContainer, styles.marginSeparator]}>
-                    <Image source={{uri: '../assets/cfpConfetti.png'}}/>
+                <View style={[styles.cfpContainer, styles.marginSeparator2]}>
+                    <Image resizeMode='contain' source={require('../assets/cfpconfetti.png')} style={styles.cfpConfetti}/>
                     <View>
                         <Text style={styles.cfpTitle}>Call for speakers</Text>
                         <Text>Apply to be a speaker</Text>
@@ -33,8 +36,29 @@ const HomeScreen = ({navigation}: NativeStackScreenProps<ParamListBase, screen_n
                         <Text>▶</Text>
                     </View>
                 </View>
-                <View style={[styles.sponsorsContainer, styles.marginSeparator]}>
-                    <Text style={styles.sponsorsContainerTitle}>Sponsors</Text>
+                <View style={[styles.sponsorsContainer, styles.marginSeparator2]}>
+                    <Text style={[styles.sponsorsContainerTitle, styles.marginSeparator]}>Sponsors</Text>
+                    <View style={[styles.sponsorsIconsContainer, styles.justifyCenter]}>
+                        <Image resizeMode='contain' source={require('../assets/1920px-Google_2015_logo.svg.png')} style={{ marginVertical: 10}}/>
+                    </View>
+                    <View style={[styles.sponsorsIconsContainer, styles.justifyBetween, styles.marginSeparator]}>
+                        <Image resizeMode='contain' source={require('../assets/Andela-logo-landscape-blue.png')}/>
+                        <Image resizeMode='contain' source={require('../assets/hover_logo.png')}/>
+                        <Image resizeMode='contain' source={require('../assets/jetbrains.png')}/>
+                    </View>
+                </View>
+                <View style={[styles.sponsorsContainer, styles.marginSeparator2]}>
+                    <Text style={[styles.sponsorsContainerTitle, styles.marginSeparator]}>Organized by :</Text>
+                    <View style={[styles.sponsorsIconsContainer, styles.justifyAround, styles.marginSeparator]}>
+                        <Android254Icon/>
+                        <Image resizeMode='contain' source={require('../assets/kotlin.png')}/>
+                        <Image resizeMode='contain' source={require('../assets/unnamed.png')}/>
+                    </View>
+                    <View style={[styles.sponsorsIconsContainer, styles.justifyAround, styles.marginSeparator]}>
+                        <AppsLabIcon/>
+                        <Image resizeMode='contain' source={require('../assets/Layer2-1.png')}/>
+                        <TiskosIcon/>
+                    </View>
                 </View>
             </ScrollView>
         </SafeAreaView>
@@ -70,11 +94,24 @@ const styles= StyleSheet.create({
     marginSeparator: {
         marginVertical: 15,
     },
+    marginSeparator2: {
+        marginVertical: 18,
+    },
+    droidconkeBanner: { 
+        width: '100%', 
+        marginVertical: -50,
+    },
+    cfpConfetti: {
+        width: '35%', 
+        marginVertical: -20
+    },
     cfpContainer: {
         backgroundColor: colors.DROIDCONKE_GREEN,
         padding: 20,
         flexDirection: 'row',
         justifyContent: 'space-between',
+        alignItems: 'center',
+        borderRadius: 10,
     },
     cfpTitle: {
         fontFamily: fonts.MONTSERRAT_BOLD,
@@ -91,6 +128,7 @@ const styles= StyleSheet.create({
     sponsorsContainer : {
         backgroundColor: colors.DROIDCONKE_PEARL,
         padding: 20,
+        borderRadius: 10,
     },
     sponsorsContainerTitle: {
         fontFamily: fonts.MONTSERRAT_BOLD,
@@ -98,6 +136,19 @@ const styles= StyleSheet.create({
         lineHeight: 20,
         color: colors.DROIDCONKE_BLUE,
         textAlign: 'center',
+    },
+    sponsorsIconsContainer: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    justifyCenter: {
+        justifyContent: 'center',
+    },
+    justifyBetween: {
+        justifyContent: 'space-between',
+    },
+    justifyAround: {
+        justifyContent: 'space-around',
     }
 })
 
