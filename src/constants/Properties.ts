2cd2d1402b9e43f185c0fe7d8d16f4ec20faf27a
brianwachira <brianwachira7@gmail.com>
2022-10-01 14:29:06 +0300

    [feat] : add common layout properties

--- src/constants/Properties.ts
+++ src/constants/Properties.ts
@@ -1,7 +1,33 @@
 // includes globally reusable CSS properties like border-radius, box-shadows e.t.c
-
+import { StyleSheet } from "react-native"
 enum properties {
     BORDER_RADIUS = 8,
     BORDER_RADIUS_MAX = 50,
     BOX_SHADOW = "",
 }
+
+// Layout properties.
+export const layoutProperties = StyleSheet.create({
+    justifyCenter: {
+        justifyContent: 'center',
+    },
+    justifyBetween: {
+        justifyContent: 'space-between',
+    },
+    justifyAround: {
+        justifyContent: 'space-around',
+    },
+    justifyEvenly: {
+        justifyContent: 'space-evenly',
+    },
+    justifyStart: {
+        justifyContent: 'flex-start',
+    },
+    flexRow: {
+        flexDirection: 'row',
+    },
+    itemsCenter: {
+        alignItems: 'center', 
+    },
+
+})
