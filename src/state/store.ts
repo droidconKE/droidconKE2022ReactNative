c267fd9cec5e92038c7af222aeb4e55a10146b20
brianwachira <brianwachira7@gmail.com>
2022-10-31 22:24:16 +0300

    [feat] : add redux stuff

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,13 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user";
 import scheduleReducer from "./schedule";
+import { userApi } from "../services/auth";
+import { setupListeners } from "@reduxjs/toolkit/dist/query";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     schedule: scheduleReducer,
+    // Add the generated reducer as a specific top-level slice
+    [userApi.reducerPath]: userApi.reducer,
   },
+  // Adding the api middleware enables caching, invalidation, polling,
+  // and other useful features of `rtk-query`.
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(userApi.middleware),
 });
+
+// optional, but required for refetchOnFocus/refetchOnReconnect behaviors
+// see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
+setupListeners(store.dispatch);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
