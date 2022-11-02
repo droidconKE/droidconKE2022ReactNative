5294e5dd13ef20fd63d10e4eefba85f411bc2281
brianwachira <brianwachira7@gmail.com>
2022-11-02 22:22:15 +0300

    [feat] : add type for pagination that can be used anywhere

---
+++ src/types/Pagination.ts
@@ -0,0 +1,11 @@
+export interface Pagination {
+    paginator: {
+        count: number,
+        per_page: number,
+        current_page: number,
+        next_page: string,
+        has_more_pages: boolean,
+        next_page_url: string | null,
+        previous_page_url: string | null,
+    }
+}
