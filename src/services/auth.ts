f439609f78b10dac0c5bc0b596cefcf9dde9d84f
brianwachira <brianwachira7@gmail.com>
2022-11-04 16:10:34 +0300

    [feat] : add getSessions query

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,5 +1,8 @@
-import { API_URL } from "@env";
+import { API_URL, EVENT_SLUG } from "@env";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { Pagination } from "../types/Pagination";
+import { RootState } from "../state/store";
+import Session from "../types/Session";
 import User from "../types/Users";
 
 interface LoginResponse {
@@ -12,11 +15,32 @@ interface LoginResponse {
 interface LoginRequest {
   access_token: string;
 }
+interface SessionsResponse<T> {
+    data?: T[];
+    meta?: Pagination;
+}
+
+interface SessionRequest {
+    per_page: string,
+    page: string,
+}
 
 // Define a service using a base URL and expected endpoints
 export const userApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: API_URL,
+
+    // add token if it exists
+        prepareHeaders: (headers, { getState }) => {
+          const token = (getState() as RootState).user.token;
+
+          // If we have a token set in state, let's assume that we should be passing it.
+          if (token) {
+              headers.set('authorization', `Bearer ${token}`);
+          }
+
+          return headers;
+      }
   }),
   endpoints: (builder) => ({
     googleSocialAuth: builder.mutation<LoginResponse, LoginRequest>({
@@ -36,9 +60,22 @@ export const userApi = createApi({
         };
       },
     }),
+    getSessions: builder.query<SessionsResponse<Session>, SessionRequest>({
+        query: ({per_page, page = "1"}) => {
+            const formData = new FormData();
+            formData.append("event_slug", EVENT_SLUG);
+            (per_page && formData.append("per_page", per_page));
+            formData.append("page", page)
+            return {
+                url: 'sessions',
+                method: 'GET',
+                body: formData,
+            }
+        }
+    })
   }),
 });
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
-export const { useGoogleSocialAuthMutation } = userApi;
+export const { useGoogleSocialAuthMutation, useGetSessionsQuery } = userApi;
