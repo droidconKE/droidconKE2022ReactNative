bdcc28ac464789341e4bbc0c3db0007b59c18314
brianwachira <brianwachira7@gmail.com>
2022-11-05 23:29:42 +0300

    [feat] : add some helper incase session data doesnt have image

--- src/components/cards/SessionCard.tsx
+++ src/components/cards/SessionCard.tsx
@@ -2,27 +2,41 @@ import React from "react";
 import {Image , View , TouchableOpacity , Text, StyleSheet, ImageSourcePropType, Dimensions} from "react-native"
 import { colors } from "../../constants/Colors";
 import { fonts } from '../../assets/fonts/fonts';
+import Session from "../../types/Session";
 
-export interface SessionCardProps {
-    id: string,
-    poster: ImageSourcePropType,
-    title: string,
-    time: string,
-    venue: string,
-}
-
-interface SessionCardPropsWithOnPress extends SessionCardProps {
+interface SessionCardPropsWithOnPress {
     onPress: () => void;
+    item: Session
 
 }
+const formatTime = (time: string): string => {
+
+    const date = new Date(time)
+
+    const formattedTime =  date.toLocaleTimeString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
+      });
+    return formattedTime;
+}
+
+const placeholder: ImageSourcePropType = require("../../assets/img/droidconkeplaceholder.png")
 
 const SessionCard = (props: SessionCardPropsWithOnPress) => {
     return (
         <TouchableOpacity style={styles.cardContainer} onPress={props.onPress}>
-            <Image source={props.poster} resizeMode="stretch" style={styles.poster}/>
+            {props.item.session_image === null ? 
+            <>
+            <Image source={placeholder} resizeMode="stretch" style={styles.poster}/>
+            </>
+            :
+            <>
+            <Image source={{ uri : props.item.session_image}} resizeMode="stretch" style={styles.poster}/>
+            </>
+            }
             <View style={styles.contentWrapper}>
-                <Text style={styles.title}>{props.title}</Text>
-                <Text style={styles.timeAndVenue}>@ {props.time} | {props.venue}</Text>
+                <Text style={styles.title} numberOfLines={2}>{props.item.title}</Text>
+                <Text style={styles.timeAndVenue}>@ {formatTime(props.item.start_date_time)} | {props.item.rooms[0].title}</Text>
             </View>
         </TouchableOpacity>
     )
@@ -58,6 +72,8 @@ const styles = StyleSheet.create({
     poster: {
         width: Dimensions.get('screen').width / 1.62, 
         height: Dimensions.get('screen').height / 7.5,
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
     }
 })
 
